a76e4079c8b1003d90fb132a3067dd18ff5d40af
Dương Bảo Hưng <duongbaohung@Duongs-MacBook-Pro.local>
2020-08-23 15:19:58 +0700

    (add): chakra, (create): PROFILE_DATA

---
+++ services/index.ts
@@ -0,0 +1,141 @@
+export const PROFILE_DATA = {
+    fullName: 'Dương Bảo Hưng',
+    lastName: 'Hưng',
+    firstName: 'Dương',
+    DOB: '18/5/2000',
+    github: 'baohungdev',
+    linkedIn: '',
+    phoneNumber: '(+84) 347461805',
+    email: {
+        primary: ['baohungdev@icloud.com', 'hungvtcvip1@gmail.com'],
+        secondary: null,
+    },
+    description: '',
+    education: {
+        Under18: [
+            {
+                school: 'Nguyen Binh Khiem High School For The Gifted',
+                startYear: 2015,
+                endYear: 2018,
+                location: {
+                    city: 'Tam Ky',
+                    province: 'Quang Nam',
+                    country: 'Viet Nam',
+                },
+                major: 'IT Major',
+                isGraduate: true,
+            },
+        ],
+        Over18: [
+            {
+                startYear: 2018,
+                school: 'University of Information Technology',
+                location: {
+                    city: 'Ho Chi Minh',
+                    province: 'Ho Chi Minh',
+                    country: 'Viet Nam',
+                },
+                major: 'E-Commerce',
+                isGraduate: false,
+            },
+            {
+                startYear: 2020,
+                endYear: 2020,
+                school: 'Nordic Coder',
+                location: {
+                    city: 'Ho Chi Minh',
+                    province: 'Ho Chi Minh',
+                    country: 'Viet Nam',
+                },
+                major: 'Advance ReactJS',
+                isGraduate: true,
+            },
+        ],
+    },
+    projects: [
+        {
+            name: 'Crwn Clothing Ecommerce site',
+            year: '2020',
+            isFinished: true,
+            img: '',
+            liveDemo: '',
+            GithubRepo: '',
+            technology: [
+                {
+                    name: 'React',
+                    for: '',
+                    icon: '',
+                },
+                {
+                    name: 'Redux',
+                    for: '',
+                    icon: '',
+                },
+                {
+                    name: 'ES6',
+                    for: '',
+                    icon: '',
+                },
+                {
+                    name: 'FireBase',
+                    for: '',
+                    icon: '',
+                },
+                {
+                    name: 'Sass',
+                    for: '',
+                    icon: '',
+                },
+            ],
+            shortDescription:
+                'The final project that I learn from a React Couse. Great UI look, so does performance',
+        },
+        {
+            name: 'Great Phone Store',
+            year: '2020',
+            isFinished: true,
+            img: '',
+            liveDemo: '',
+            GithubRepo: '',
+            technology: [
+                {
+                    name: 'Nextjs',
+                    for: '',
+                    icon: '',
+                },
+                {
+                    name: 'TypeScript',
+                    for: '',
+                    icon: '',
+                },
+                {
+                    name: 'React hooks',
+                    for: '',
+                    icon: '',
+                },
+                {
+                    name: 'Chakra-UI',
+                    for: '',
+                    icon: '',
+                },
+                {
+                    name: 'Styled-components',
+                    for: '',
+                    icon: '',
+                },
+                {
+                    name: 'GraphQL',
+                    for: '',
+                    icon: '',
+                },
+                {
+                    name: 'Apollo',
+                    for: '',
+                    icon: '',
+                },
+            ],
+            shortDescription:
+                'Another E-Commerce site but using Mobile First approach, Sever side Rendering, Context API ',
+        },
+    ],
+}
